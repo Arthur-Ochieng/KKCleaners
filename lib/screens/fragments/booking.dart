59b35b563ef3245106df2e72c01111d25d8a99a0
Arthur-Ochieng <johnarthur016@gmail.com>
2022-03-15 03:13:38 +0300

    added booking screen

--- lib/screens/fragments/booking.dart
+++ lib/screens/fragments/booking.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:cleaner/screens/fragments/confirmation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class BookingPage extends StatefulWidget {
   const BookingPage({Key? key}) : super(key: key);
@@ -8,6 +12,19 @@ class BookingPage extends StatefulWidget {
 }
 
 class _BookingPageState extends State<BookingPage> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
   List<String> bottomMenus = [
     "Mini",
     "Sedan",
@@ -31,8 +48,13 @@ class _BookingPageState extends State<BookingPage> {
       width: double.maxFinite,
       child: Stack(
         children: [
-          Center(
-            child: Text("Google Map"),
+          GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+            zoomControlsEnabled: false,
           ),
           Positioned(
             top: 40,
@@ -86,55 +108,78 @@ class _BookingPageState extends State<BookingPage> {
             bottom: 10,
             left: 20,
             right: 20,
-            child: Card(
-              elevation: 6,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(100),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 8.0, right: 8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: List.generate(bottomMenus.length, (index) {
-                    return GestureDetector(
-                      onTap: (){
-                        setState(() {
-                          selectedIndex = index;
-                        });
-                      },
-                      child: AnimatedContainer(
-                        duration: const Duration(milliseconds: 500),
-                        height: 50,
-                        padding: const EdgeInsets.only(left: 15, right: 15),
-                        decoration: BoxDecoration(
-                          color: selectedIndex == index ? const Color(0xff3C2593): Colors.white,
-                          borderRadius: BorderRadius.circular(100),
-                        ),
-                        child: Row(
-                          children: [
-                            selectedIndex == index
-                              ? Icon(
-                                  bottomMenusIcons[index],
-                                  color: Colors.white,
-                                  size: 30,
-                                )
-                              : Container(),
-                            SizedBox(
-                              width: selectedIndex == index ? 5 : 0,
+            child: Column(
+              children: [
+                Card(
+                  elevation: 6,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8.0, right: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: List.generate(bottomMenus.length, (index) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedIndex = index;
+                            });
+                          },
+                          child: AnimatedContainer(
+                            duration: const Duration(milliseconds: 500),
+                            height: 50,
+                            padding: const EdgeInsets.only(left: 15, right: 15),
+                            decoration: BoxDecoration(
+                              color: selectedIndex == index
+                                  ? const Color(0xff3C2593)
+                                  : Colors.white,
+                              borderRadius: BorderRadius.circular(100),
                             ),
-                            Text(
-                              bottomMenus[index],
-                              style: TextStyle(
-                                color: selectedIndex == index ? Colors.white : const Color(0xff3C2593),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    );
-                  }),
+                            child: Row(
+                              children: [
+                                selectedIndex == index
+                                    ? Icon(
+                                        bottomMenusIcons[index],
+                                        color: Colors.white,
+                                        size: 30,
+                                      )
+                                    : Container(),
+                                SizedBox(
+                                  width: selectedIndex == index ? 5 : 0,
+                                ),
+                                Text(
+                                  bottomMenus[index],
+                                  style: TextStyle(
+                                    color: selectedIndex == index
+                                        ? Colors.white
+                                        : const Color(0xff3C2593),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      }),
+                    ),
+                  ),
                 ),
-              ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => ConfirmationPage()
+                        )
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xff3C2593),
+                    ),
+                    child: const Center(
+                      child: Text("Ride Now"),
+                    )),
+              ],
             ),
           ),
         ],
